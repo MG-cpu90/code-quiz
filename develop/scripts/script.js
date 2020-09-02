9b37aba6637c1fdb8e5bd223dfe2431537d29a9f
Massimo Guida <massimo.guidamusic@gmail.com>
2020-09-02 11:07:35 -0400

    added comment for timer deduction for wrong answers

--- develop/scripts/script.js
+++ develop/scripts/script.js
@@ -218,6 +218,7 @@ $(document).click(function(event) {
         // Append the text to <li>
         assessment1ul.append(textWrong);
 
+        // Deduct 10 seconds from the timer if answer is incorrect
         secondsLeft -= 10;
         timeContainer.textContent = "Time: " + secondsLeft;
 
